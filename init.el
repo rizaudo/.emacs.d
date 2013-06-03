9549c1f9bd9695cd2dfc244a9b6f2e9de055b27d
rizaudo <hosoyakei@gmail.com>
2013-06-03 16:21:25 +0900

    update init.el

--- init.el
+++ init.el
@@ -1,13 +1,13 @@
-;;デフォルトランゲージ　日本語
+
 (set-language-environment 'Japanese)
 
 (setq default-major-mode 'text-mode)
 
-(set-keyboard-coding-system 'sjis)
-(prefer-coding-system 'utf-8)
-;コンパイル時　cl使用　
+(set-keyboard-coding-system 'euc-jp)
+(prefer-coding-system 'euc-jp)
+　
 (require 'cl)
-;package設定
+
 (require 'package)
 (add-to-list 'package-archives
     '("marmalade" .
@@ -15,20 +15,22 @@
 (package-initialize)
 
 
-;括弧対応強調
+
 (show-paren-mode t)
-;カーソル位置の列と行数表示
+
 (line-number-mode t)
 (column-number-mode t)
-;時間を２４時表記で表示
+
 (setq display-time-day-and-date t)
 (setq display-time-24hr-format t)
 (display-time)
 
 (setq read-file-name-completion-ignore-case t)
 
-;;黒字に白地
-(set-foreground-color "white")
-(set-background-color "black")
-(set-cursor-color "white")
+
+
+(set-background-color "Black")
+(set-foreground-color "White")
+(set-cursor-color "White")
+(set-frame-parameter nil 'alpha 90)
 
