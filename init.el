c99c6ea6e959cfc32e1c8742f9e927741ec282f2
kei hosoya <hosoyakei@gmail.com>
2013-11-19 18:04:13 +0900

    fix some macros

--- init.el
+++ init.el
@@ -1,35 +1,44 @@
 ;;; emacs-setup Sat Nov  9 12:29:18 2013
 ;;; for emacs 24.3(now)
+;;; 基本的に例外が無い限り最新版を追うこととします
 ;;; 想定環境 Linux(Debian) Mac Windows
 ;;; 外部に投げる場合や環境依存の物を使う場合、system-typeを見てunless
 ;;; かwhenしましょう。 unlessよりは(when (not (eq system-type 'hoge)))
 ;;; のほうが可読性良いかもしれない。
+;;; 使っているシステムの情報
+;;; mac:darwin windows:windows-nt
 (set-language-environment 'Japanese)
 (setq default-major-mode 'text-mode)
 (set-keyboard-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
 ;use commonlisp
-(with-no-warnings
+(eval-when-compile
   (require 'cl))
 
-;;package config
-(require 'package)
-(add-to-list 'package-archives
+;;; def macro
+
+(defmacro req (lib &rest body)
+  `(when (locate-library ,(symbol-name lib))
+     (require ',lib) ,@body))
+
+;;;  alias emacs = emacsclient
+(req server
+  (unless (server-running-p)
+  (server-start)))
+
+
+;;; package config
+(req package 
+  (add-to-list 'package-archives
              '("marmalade" .
                "http://marmalade-repo.org/packages/")
              '("org" .
                "http://orgmode,org/elpa/"))
-(add-to-list 'package-archives
+  (add-to-list 'package-archives
              '("melpa" .
                "http://melpa.milkbox.net/packages/"))
-(package-initialize)
-
-;;;  alias emacs = emacsclient
-(require 'server)
-(unless (server-running-p)
-  (server-start))
-(setq gc-cons-threshold (* 50 gc-cons-threshold))
+  (package-initialize)) 
 
 ;;外部へのIOはコストが高すぎる。　二度目のevalがされないように何かしらの対策をすることが必要である。
 (package-refresh-contents)
@@ -56,6 +65,7 @@
     undo-tree
     yasnippet
 
+    auto-complete
     ;; helm packages
     helm
     
@@ -70,7 +80,6 @@
   (when (not (package-installed-p package))
     (package-install package)))
 
-
 (when (eq system-type 'darwin)
   (set-face-attribute 'default nil :family "Consolas")
   (set-fontset-font "fontset-default"
@@ -80,14 +89,25 @@
                     'katakana-jisx0201
                     '("Hiragino Maru Gothic ProN")))
 
-(require 'color-theme)
-(load-theme 'misterioso t)
+(req color-theme
+  (load-theme 'misterioso t))
 
+
+;;; terminal
 
 ;;multi-tarm setting
-(require 'multi-term)
-(setq multi-term-program "/bin/zsh")
+(req multi-term
+  (setq multi-term-program "/bin/zsh"))
 
+(setq eshell-command-aliases-list
+      (append
+       (list
+        (list "ls" "ls -a"))
+        eshell-command-aliases-list))
+
+;;; 詳細設定
+
+(setq gc-cons-threshold (* 50 gc-cons-threshold))
 ;;; other settings 
 ;;; あのビープ音を削除する
 (setq visible-bell t)
@@ -109,19 +129,20 @@
 
 (setq read-file-name-completion-ignore-case t)
 
+;;; C-kで行全体を削除
+(setq kill-whole-line t)
 
 
+;;; ウインドウの外見設定
 ;;;(set-background-color "Black")
 ;;;(set-foreground-color "White")
 ;;;(set-cursor-color "White")
 (set-frame-parameter nil 'alpha 85)
-
 (set-cursor-color "Blue")
 
+(require 'whitespace)
 ;;; kill -9 fuckin em-space!!!!!!!!!!!!!!!!!!
 (global-whitespace-mode 1)
-
-(require 'whitespace)
 (setq whitespace-style '(face tabs tab-mark spaces space-mark))
 (setq whitespace-display-mappings
        ;; all numbers are Unicode codepoint in decimal. try (insert-char 182 ) to see it
@@ -138,8 +159,8 @@
 
 
 ;;;programming support 
-(require 'undo-tree)
-(global-undo-tree-mode)
+(req undo-tree
+(global-undo-tree-mode))
 
 (require 'auto-complete)
 (require 'auto-complete-config)
@@ -148,7 +169,7 @@
 (add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
 (add-hook 'clojure-nrepl-mode-hook 'ac-nrepl-setup)
 
-(require 'flymake)
+;;; (require 'flymake)
 
 ;;src package setting
 
@@ -160,9 +181,10 @@
 (setq bongo-enabled-backends '(vlc))
 
 ;; howm
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/howm/")
-(setq howm-menu-lang 'ja)
-(require 'howm-mode)
+(when (eq system-type 'darwin)
+  (add-to-list 'load-path "/usr/share/emacs/site-lisp/howm/")
+  (setq howm-menu-lang 'ja)
+  (require 'howm-mode))
 
 ;; my function
 (when (eq system-type 'darwin)
@@ -170,5 +192,9 @@
     (interactive)
     (shell-command (concat "open ."))))
 
+;;; play-sound周りはわけ分からん
+
 ;;; (play-sound :file sound-current-dic+hoge :volume 0.3)
 ;;; (setq sound-current-dic "/Users/keihosoya/.emacs.d/sound")
+;;; (add-hook 'after-save-hook '(lambda  (play-sound-file "~/.emacs.d/wav/SAMUEAI.wav")))
+
