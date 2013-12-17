ded5e89643401c0b867b6c73a744302213f8f1d5
kei hosoya <hosoyakei@gmail.com>
2013-12-17 17:02:07 +0900

    fix flymake setting

--- init.el
+++ init.el
@@ -207,8 +207,16 @@
 (req clojure-snippets
      (clojure-snippets-initialize))
 
-(require 'flymake)
-(add-hook 'java-mode-hook 'flymake-mode-on)
+(req flymake
+     (add-hook 'java-mode-hook 'flymake-mode-on))
+
+;;; test code
+(defun my-java-flymake-init ()
+  (list "javac" (list (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-with-folder-structure))))
+(add-to-list 'flymake-allowed-file-name-masks '("\\.java$" my-java-flymake-init flymake-simple-cleanup))
+
+
 
 ;;src package setting
 
