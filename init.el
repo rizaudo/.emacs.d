d7fa6a3743f0cd4b94dfe73bd45ef54b052bf28f
rizaudo <hosoyakei@gmail.com>
2014-03-25 11:07:39 +0900

    change font in mac

--- init.el
+++ init.el
@@ -134,13 +134,20 @@
 
 
 (when (eq system-type 'darwin)
-  (set-face-attribute 'default nil :family "Consolas")
+  (set-face-attribute 'default nil :family "Ricty Diminished")
   (set-fontset-font "fontset-default"
                     'japanese-jisx0208
-                    '("Hiragino Maru Gothic ProN"))
+                    '("Ricty Diminished"))
   (set-fontset-font "fontset-default"
                     'katakana-jisx0201
-                    '("Hiragino Maru Gothic ProN")))
+                    '("Ricty Diminished"))
+  ;; (set-fontset-font "fontset-default"
+  ;;                   'japanese-jisx0208
+  ;;                   '("Hiragino Maru Gothic ProN"))
+  ;; (set-fontset-font "fontset-default"
+  ;;                   'katakana-jisx0201
+  ;;                   '("Hiragino Maru Gothic ProN"))
+  )
 
 
 ;;; pref setting  by Dr.Yas
