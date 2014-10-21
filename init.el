fd375dbe7db3dc8748fc0f70d5b04caf2a2c56c0
rizaudo <hosoyakei@gmail.com>
2014-10-22 01:41:13 +0900

    eww

--- init.el
+++ init.el
@@ -456,7 +456,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(eww-search-prefix "https://google.com/?q="))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
