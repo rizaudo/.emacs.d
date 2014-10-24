32149ae2496c26e678a66a020617e283c5566697
rizaudo <hosoyakei@gmail.com>
2014-10-24 18:09:55 +0900

    fix agda mode

--- init.el
+++ init.el
@@ -386,6 +386,8 @@
   (add-to-list 'exec-path "~/Library/Haskell/bin/")
   (load-file (let ((coding-system-for-read 'utf-8))
                (shell-command-to-string "agda-mode locate")))
+  (setq agda2-include-dirs
+        (list "." (expand-file-name "~/AgdaLibrary/")))
   
   )
 
