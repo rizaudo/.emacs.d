498cb20f923b21101899f87e20c2597ab1c30f11
rizaudo <hosoyakei@gmail.com>
2014-12-24 18:32:37 +0900

    add YaTeX

--- init.el
+++ init.el
@@ -481,6 +481,11 @@
   (add-to-list 'load-path "/usr/share/emacs/site-lisp/howm/")
   (req howm
        (setq howm-menu-lang 'ja))
+  ;; YaTeX
+  (setq load-path (cons (expand-file-name "~/.emacs.d/yatex1.77/") load-path))
+  (setq auto-mode-alist
+        (cons (cons "\\.tex$" 'yatex-mode) auto-mode-alist))
+  (autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
   ;; coq
   (add-to-list 'exec-path "/usr/local/bin/")
   (add-to-list 'load-path "/usr/local/opt/coq/lib/emacs/site-lisp/")
