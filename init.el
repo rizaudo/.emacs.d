b2aed791cbad8d230b25ece52fd6c09babca85c2
rizaudo <hosoyakei@gmail.com>
2014-12-24 18:10:51 +0900

    update rainbow-delimiters-color

--- init.el
+++ init.el
@@ -572,13 +572,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(rainbow-delimiters-depth-1-face ((t (:foreground "White"))))
- '(rainbow-delimiters-depth-2-face ((t (:foreground "dark red"))))
- '(rainbow-delimiters-depth-3-face ((t (:foreground "dark magenta"))))
- '(rainbow-delimiters-depth-4-face ((t (:foreground "light green"))))
+ '(rainbow-delimiters-depth-1-face ((t (:background "blue3" :foreground "White" :inverse-video t :strike-through t :overline t :weight bold))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "chartreuse3"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "alice blue"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "dodger blue"))))
  '(rainbow-delimiters-depth-5-face ((t (:foreground "dark blue"))))
  '(rainbow-delimiters-depth-6-face ((t (:foreground "forest green"))))
- '(rainbow-delimiters-depth-7-face ((t (:foreground "cyan4")))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "cyan4"))))
+ '(rainbow-delimiters-unmatched-face ((t (:foreground "red" :box (:line-width 2 :color "VioletRed4" :style released-button))))))
 
 
 ;;; font-lock Experience
