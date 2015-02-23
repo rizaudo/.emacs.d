28c4f1f3d7b17a5e5f41b61521a4d78b98dc836a
rizaudo <hosoyakei@gmail.com>
2015-02-23 22:45:04 +0900

    change ac-cider

--- init.el
+++ init.el
@@ -411,9 +411,7 @@
 (req ac-cider
      (add-hook 'cider-mode-hook 'ac-flyspell-workaround)
      (add-hook 'cider-mode-hook 'ac-cider-setup)
-     (add-hook 'cider-repl-mode-hook 'ac-cider-setup)
-     (eval-after-load "auto-complete"
-       '(add-to-list 'ac-modes 'cider-mode)))
+     (add-hook 'cider-repl-mode-hook 'ac-cider-setup))
 
 ;; (req yasnippet
 ;;      (yas-global-mode 0))
