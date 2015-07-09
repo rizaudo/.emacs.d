0433e0233d348dfd8f441ee89ec0e9cc7e0fc2dd
rizaudo <hosoyakei@gmail.com>
2015-07-09 21:58:10 +0900

    change raibow-delimiters face 1,3

--- init.el
+++ init.el
@@ -655,9 +655,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(rainbow-delimiters-depth-1-face ((t (:background "blue3" :foreground "White" :inverse-video t :strike-through t :overline t :weight bold))))
+ '(rainbow-delimiters-depth-1-face ((t (:background "blue3" :foreground "White" :strike-through t :overline t :weight bold))))
  '(rainbow-delimiters-depth-2-face ((t (:foreground "chartreuse3"))))
- '(rainbow-delimiters-depth-3-face ((t (:foreground "alice blue"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "blue2"))))
  '(rainbow-delimiters-depth-4-face ((t (:foreground "dodger blue"))))
  '(rainbow-delimiters-depth-5-face ((t (:foreground "dark blue"))))
  '(rainbow-delimiters-depth-6-face ((t (:foreground "forest green"))))
